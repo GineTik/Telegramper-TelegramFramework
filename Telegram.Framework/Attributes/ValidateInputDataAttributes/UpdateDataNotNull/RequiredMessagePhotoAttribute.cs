da54521956a7237d0db2ac347fda716e82a6742a
GineTik <densheva18072003@gmail.com>
2023-07-27 21:47:14 +0300

    Renamed validation attribute

---
+++ Telegram.Framework/Attributes/ValidateInputDataAttributes/UpdateDataNotNull/RequiredMessagePhotoAttribute.cs
@@ -0,0 +1,10 @@
+﻿namespace Telegram.Framework.Attributes.ValidateInputDataAttributes.UpdateDataNotNull
+{
+    public class RequiredMessagePhotoAttribute : UpdateDataNotNullAttribute
+    {
+        public RequiredMessagePhotoAttribute() : base(update => update.Message?.Photo)
+        {
+            ErrorMessage = "The photo of the message is required";
+        }
+    }
+}
---
+++ Telegram.Framework/Attributes/ValidateInputDataAttributes/UpdateDataNotNull/RequiredMessageTextAttribute.cs
@@ -0,0 +1,10 @@
+﻿namespace Telegram.Framework.Attributes.ValidateInputDataAttributes.UpdateDataNotNull
+{
+    public class RequiredMessageTextAttribute : UpdateDataNotNullAttribute
+    {
+        public RequiredMessageTextAttribute() : base(update => update.Message?.Text)
+        {
+            ErrorMessage = "The text of the message is required";
+        }
+    }
+}
