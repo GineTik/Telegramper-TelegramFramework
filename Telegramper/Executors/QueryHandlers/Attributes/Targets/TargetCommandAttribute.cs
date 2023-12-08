5a71ad06718a2e7fa7e6ec7e6cd43297d11601c8
GineTik <densheva18072003@gmail.com>
2023-12-08 13:31:15 +0200

    Updated Command and CallbackData attributes

--- Telegramper/Executors/QueryHandlers/Attributes/Targets/TargetCommandAttribute.cs
+++ Telegramper/Executors/QueryHandlers/Attributes/Targets/TargetCommandAttribute.cs
@@ -1,6 +1,7 @@
 ﻿using System.Text.RegularExpressions;
 using Telegram.Bot.Types;
 using Telegram.Bot.Types.Enums;
+using Telegramper.Executors.Common.Models;
 using Telegramper.Executors.QueryHandlers.Attributes.BaseAttributes;
 
 namespace Telegramper.Executors.QueryHandlers.Attributes.Targets
@@ -8,30 +9,27 @@ namespace Telegramper.Executors.QueryHandlers.Attributes.Targets
     [TargetUpdateType(UpdateType.Message)]
     public class TargetCommandAttribute : TargetAttribute
     {
-        public string[] Commands { get; set; }
         public string? Description { get; set; }
+        public bool? Visible { get; set; }
+        public string? Key { get; set; }
+        
+        private string? _command;
 
-        public TargetCommandAttribute(string? commands = null)
+        public TargetCommandAttribute(string? command = null)
         {
-            Commands = commands?.Replace(" ", "").Split(',')
-                ?? new string[0];
+            _command = command;
         }
 
         public override bool IsTarget(Update update)
         {
             var text = update.Message!.Text;
+            
             if (text == null)
             {
                 return false;
             }
-
-            string command = takeCommandFromText(text);
-            if (Commands.Length == 0)
-            {
-                return command == TransformedMethodName;
-            }
-
-            return Commands.Contains(command);
+            
+            return _command == takeCommandFromText(text);
         }
 
         private static string takeCommandFromText(string text)
@@ -39,6 +37,11 @@ namespace Telegramper.Executors.QueryHandlers.Attributes.Targets
             var command = text.Split(' ').First().TrimStart('/');
             command = Regex.Replace(command, "@\\w+", ""); // remove username
             return command;
+        }
+
+        protected override void Initialization(ExecutorMethod method)
+        {
+            _command ??= TransformedMethodName;
         }
     }
 }
