88ff38d9c4cba139874aae6ce0148f69b1548160
GineTik <densheva18072003@gmail.com>
2024-01-18 18:21:41 +0200

    Added new methods (BackAsync and ShiftAsync)

--- Telegramper/Sequence/Service/ISequenceService.cs
+++ Telegramper/Sequence/Service/ISequenceService.cs
@@ -7,9 +7,9 @@ namespace Telegramper.Sequence.Service
         Task StartAsync(string dialogName);
         Task StartAsync<T>() where T : Executor;
         Task StartAsync(Type sequenceType);
-        Task NextAsync();
-
-
+        Task NextAsync(int steps = 1);
+        Task BackAsync(int steps = 1);
+        Task ShiftAsync(int offset = 1);
         Task EndAsync(bool executeEndCallback = true);
         Task<bool> IsLaunchedAsync();
     }
